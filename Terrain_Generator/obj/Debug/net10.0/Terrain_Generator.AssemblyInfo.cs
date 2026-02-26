809deea451af7b402e01c45ff88589d2b33d91a5
Edw1n-G <edwin.grams909@gmail.com>
2026-02-26 20:47:05 +0100

    .gitignore

--- Terrain_Generator/obj/Debug/net10.0/Terrain_Generator.AssemblyInfo.cs
+++ Terrain_Generator/obj/Debug/net10.0/Terrain_Generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Terrain_Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2f1e418b87ae8ec7860cffd3b687b002a8aca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Terrain_Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Terrain_Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
